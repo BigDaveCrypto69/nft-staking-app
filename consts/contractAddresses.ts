b03e882dec0079a83da2107fb47ce612bce880e2
BigDaveCrypto69 <160251206+BigDaveCrypto69@users.noreply.github.com>
2024-03-27 23:04:31 +0000

    Update contractAddresses.ts

--- consts/contractAddresses.ts
+++ consts/contractAddresses.ts
@@ -1,6 +1,6 @@
 export const nftDropContractAddress =
-  "0x017E2B2821ff0adE1A69fE2891044E14B8900021";
+  "0x500bfA1CcF2d4cA47cbe115B6B52dE6541f82e84";
 export const tokenContractAddress =
   "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";
 export const stakingContractAddress =
-  "0xabB3baBd700DBDdCBb8d3a86859144CE440b94d4";
+  "0x8bC8af12810bb4b33E80ed1e6162AB0aC0bEf045";
